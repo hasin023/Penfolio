4c13eccb6f708c26612500fe3b69cae49cd3d8c1
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-02 13:32:23 +0600

    Inserting images to db and showing

--- index.php
+++ index.php
@@ -27,7 +27,8 @@
             while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
+                $post_date = DateTime::createFromFormat('Y-m-d', $row['post_date']);
+                $post_date = $post_date->format('F d, Y');
                 $post_image = $row['post_image'];
                 $post_content = $row['post_content'];
 
@@ -44,6 +45,8 @@
                         by <a href='index.php'>$post_author</a>
                     </p>
                     <p><span class='glyphicon glyphicon-time'></span> Posted on $post_date</p>
+                    <hr>
+                    <img class='img-responsive' src='images/$post_image' alt='Blog Post Image'>
                     <hr>
                     <p>$post_content</p>
                     <a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>
