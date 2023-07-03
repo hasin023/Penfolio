b5ee232e8802af665692384d14098a84fae6f769
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-03 14:26:36 +0600

    Working on Organization

--- admin_2/register.php
+++ admin_2/register.php
@@ -14,15 +14,15 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Register</title>
+    <title>Register</title>
 
-    <!-- Custom fonts for this template-->
+    <!-- Custom fonts-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-    <!-- Custom styles for this template-->
+    <!-- Custom styles-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
