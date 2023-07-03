7bc03b1c944cbb510e414f37705fa2c94b8dcff5
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-03 20:02:07 +0600

    Reading category title from button to input box

--- admin/categories.php
+++ admin/categories.php
@@ -60,6 +60,40 @@
                             </div>
 
                         </form>
+
+                        <form action="" method="post">
+
+                            <div class="form-group">
+
+                            <?php
+
+                            if (isset($_GET['edit'])) {
+                                $edit_cat_id = $_GET['edit'];
+
+                                $query = "SELECT * FROM categories WHERE cat_id = {$edit_cat_id} ";
+                                $select_edit_categories = mysqli_query($connection, $query);
+
+
+                                while ($row = mysqli_fetch_assoc($select_edit_categories)) {
+                                    $cat_id = $row['cat_id'];
+                                    $cat_title = $row['cat_title'];
+
+                                    ?>
+                                    <input value="<?php if (isset($cat_title))
+                                        echo $cat_title; ?>" class="form-control bg-light border-1 small" type="text" name="cat_title">
+                                <?php }
+                            }
+
+                            ?>
+
+
+                            </div>
+
+                            <div class="form-group">
+                                <input class="btn btn-warning" type="submit" name="submit" value="Update">
+                            </div>
+
+                        </form>
                     </div>
                     <!-- Add Categories Ended-->
 
@@ -73,11 +107,11 @@
                         <div class="card-body">
                             <div class="table-responsive">
 
-                                <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
                                             <th class="text-dark">ID</th>
-                                            <th class="text-dark">Category Title</th>
+                                            <th class="text-dark text-center">Category Title</th>
                                             <!-- <th class="text-dark">Delete Category</th> -->
                                         </tr>
                                     </thead>
@@ -93,9 +127,10 @@
                                         $cat_title = $row['cat_title'];
 
                                         echo "<tr>
-                                                <td class='text-dark'>$cat_id</td>
+                                                <td width='10%' class='text-dark'>$cat_id</td>
                                                 <td class='text-dark'>$cat_title</td>
-                                                <td><a href='categories.php?delete={$cat_id}' class='btn btn-danger'>DELETE</a></td>
+                                                <td width='5%'><a href='categories.php?edit={$cat_id}' class='btn btn-warning'>EDIT</a></td>
+                                                <td width='5%'><a href='categories.php?delete={$cat_id}' class='btn btn-danger'>DELETE</a></td>
                                             </tr>";
                                     }
 
@@ -105,9 +140,9 @@
                                     //Deleting categories
                                     
                                     if (isset($_GET['delete'])) {
-                                        $the_cat_id = $_GET['delete'];
+                                        $delete_cat_id = $_GET['delete'];
 
-                                        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+                                        $query = "DELETE FROM categories WHERE cat_id = {$delete_cat_id} ";
                                         $delete_category = mysqli_query($connection, $query);
 
                                         header("Location: categories.php");
