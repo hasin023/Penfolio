ba888707a1c1db416650ea7efafd0480e30587ea
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-06 17:49:33 +0600

    Fixed add post insert bug

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -19,7 +19,7 @@ if (isset($_POST['create_post'])) {
 
     $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_counts, post_status) ";
 
-    $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_comment_counts}' , '{$post_tags}', '{$post_status}') ";
+    $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}' , '{$post_comment_counts}', '{$post_status}') ";
 
     $create_post_query = mysqli_query($connection, $query);
 
