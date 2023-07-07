3cd3eaea7d82ed4110ed38bee53ae069c801f7ef
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-07 18:11:01 +0600

    Adding sessions to the admin panel

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,6 +1,21 @@
-<?php ob_start() ?>
+<?php ob_start(); ?>
+<?php session_start(); ?>
 <?php include("../includes/db.php"); ?>
 <?php include("../functions.php"); ?>
+
+
+<?php
+
+if (!isset($_SESSION['user_role'])) {
+    header("Location: ../index.php");
+} else if (isset($_SESSION['user_role'])) {
+    if ($_SESSION['user_role'] !== 'admin') {
+        header("Location: ../index.php");
+    }
+}
+
+
+?>
 
 
 <!DOCTYPE html>
--- admin/includes/admin_topbar.php
+++ admin/includes/admin_topbar.php
@@ -60,7 +60,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Hasin Mahtab</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['lastname']; ?></span>
                                 <img class="img-profile rounded-circle"
                                     src="images/userIcon.png">
                             </a>
