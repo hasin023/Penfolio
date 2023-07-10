b3f0f48c50583b44eff6b50e812a55408f01fcd3
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-11 00:38:16 +0600

    Fixed slight bugs in categories

--- admin/categories.php
+++ admin/categories.php
@@ -65,7 +65,7 @@
                         <div class="card-body">
                             <div class="table-responsive">
 
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
                                             <th class="text-dark">ID</th>
--- admin/includes/admin_topbar.php
+++ admin/includes/admin_topbar.php
@@ -60,9 +60,23 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['username']; ?></span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php
+
+                                if (isset($_SESSION['username'])) {
+
+                                    echo $_SESSION['username'];
+                                }
+
+                                ?></span>
                                 <img class="img-profile rounded-circle"
-                                    src="images/users/<?php echo $_SESSION['user_image']; ?>">
+                                    src="images/users/<?php
+
+                                    if (isset($_SESSION['user_image'])) {
+
+                                        echo $_SESSION['user_image'];
+                                    }
+
+                                    ?>">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
