51efa3e0457439af04f33137263b13bc1cb1c604
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-12 13:55:31 +0600

    Added single post page

--- blogy/single.php
+++ blogy/single.php
@@ -2,22 +2,55 @@
 
 <?php include("includes/navigation.php"); ?>
 
-  <div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('images/hero_5.jpg');">
-    <div class="container">
-      <div class="row same-height justify-content-center">
-        <div class="col-md-6">
-          <div class="post-entry text-center">
-            <h1 class="mb-4">Don’t assume your user data in the cloud is safe</h1>
-            <div class="post-meta align-items-center text-center">
-              <figure class="author-figure mb-0 me-3 d-inline-block"><img src="images/person_1.jpg" alt="Image" class="img-fluid"></figure>
-              <span class="d-inline-block mt-1">By Carl Atkinson</span>
-              <span>&nbsp;-&nbsp; February 10, 2019</span>
+<?php
+
+if (isset($_GET['p_id'])) {
+  $the_post_id = $_GET['p_id'];
+}
+
+$view_query = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = $the_post_id";
+$send_query = mysqli_query($connection, $view_query);
+
+
+if (!$send_query) {
+  die("QUERY FAILED" . mysqli_error($connection));
+}
+
+$query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+
+$select_post_query = mysqli_query($connection, $query);
+
+while ($row = mysqli_fetch_assoc($select_post_query)) {
+  $post_id = $row['post_id'];
+  $post_title = $row['post_title'];
+  $post_author = $row['post_author'];
+  $date = DateTime::createFromFormat('Y-m-d', $row['post_date']);
+  $post_date = $date->format('F d, Y');
+  $post_image = $row['post_image'];
+  $post_content = $row['post_content'];
+
+
+  echo "  
+      <div class='site-cover site-cover-sm same-height overlay single-page' style='background-image: url(\"images/$post_image\");'>
+        <div class='container'>
+          <div class='row same-height justify-content-center'>
+            <div class='col-md-6'>
+              <div class='post-entry text-center'>
+                <h1 class='mb-4'>$post_title</h1>
+                <div class='post-meta align-items-center text-center'>
+                  <span class='d-inline-block mt-1'>By $post_author</span>
+                  <span>&nbsp;-&nbsp; $post_date</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
+      </div>";
+
+}
+
+?>
+
 
   <section class="section">
     <div class="container">
@@ -27,31 +60,9 @@
         <div class="col-md-12 col-lg-8 main-content">
 
           <div class="post-content-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium nam quas inventore, ut iure iste modi eos adipisci ad ea itaque labore earum autem nobis et numquam, minima eius. Nam eius, non unde ut aut sunt eveniet rerum repellendus porro.</p>
-            <p>Sint ab voluptates itaque, ipsum porro qui obcaecati cumque quas sit vel. Voluptatum provident id quis quo. Eveniet maiores perferendis officia veniam est laborum, expedita fuga doloribus natus repellendus dolorem ab similique sint eius cupiditate necessitatibus, magni nesciunt ex eos.</p>
-            <p>Quis eius aspernatur, eaque culpa cumque reiciendis, nobis at earum assumenda similique ut? Aperiam vel aut, ex exercitationem eos consequuntur eaque culpa totam, deserunt, aspernatur quae eveniet hic provident ullam tempora error repudiandae sapiente illum rerum itaque voluptatem. Commodi, sequi.</p>
-            <div class="row my-4">
-              <div class="col-md-12 mb-4">
-                <img src="images/hero_1.jpg" alt="Image placeholder" class="img-fluid rounded">
-              </div>
-              <div class="col-md-6 mb-4">
-                <img src="images/img_2_horizontal.jpg" alt="Image placeholder" class="img-fluid rounded">
-              </div>
-              <div class="col-md-6 mb-4">
-                <img src="images/img_3_horizontal.jpg" alt="Image placeholder" class="img-fluid rounded">
-              </div>
-            </div>
-            <p>Quibusdam autem, quas molestias recusandae aperiam molestiae modi qui ipsam vel. Placeat tenetur veritatis tempore quos impedit dicta, error autem, quae sint inventore ipsa quidem. Quo voluptate quisquam reiciendis, minus, animi minima eum officia doloremque repellat eos, odio doloribus cum.</p>
-            <p>Temporibus quo dolore veritatis doloribus delectus dolores perspiciatis recusandae ducimus, nisi quod, incidunt ut quaerat, magnam cupiditate. Aut, laboriosam magnam, nobis dolore fugiat impedit necessitatibus nisi cupiditate, quas repellat itaque molestias sit libero voluptas eveniet omnis illo ullam dolorem minima.</p>
-            <p>Porro amet accusantium libero fugit totam, deserunt ipsa, dolorem, vero expedita illo similique saepe nisi deleniti. Cumque, laboriosam, porro! Facilis voluptatem sequi nulla quidem, provident eius quos pariatur maxime sapiente illo nostrum quibusdam aliquid fugiat! Earum quod fuga id officia.</p>
-            <p>Illo magnam at dolore ad enim fugiat ut maxime facilis autem, nulla cumque quis commodi eos nisi unde soluta, ipsa eius aspernatur sint atque! Nihil, eveniet illo ea, mollitia fuga accusamus dolor dolorem perspiciatis rerum hic, consectetur error rem aspernatur!</p>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus magni explicabo id molestiae, minima quas assumenda consectetur, nobis neque rem, incidunt quam tempore perferendis provident obcaecati sapiente, animi vel expedita omnis quae ipsa! Obcaecati eligendi sed odio labore vero reiciendis facere accusamus molestias eaque impedit, consequuntur quae fuga vitae fugit?</p>
-          </div>
-
-
-          <div class="pt-5">
-            <p>Categories:  <a href="#">Food</a>, <a href="#">Travel</a>  Tags: <a href="#">#manila</a>, <a href="#">#asia</a></p>
+            <p><?php echo $post_content ?></p>
+           
           </div>
 
 
