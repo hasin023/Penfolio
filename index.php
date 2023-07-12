bdbf7c7a40e814962f5780be98b041c2a44f88c4
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-07-12 23:51:44 +0600

    Separated pagination

--- index.php
+++ index.php
@@ -78,18 +78,8 @@
         ?>
 
 
-          <div class="row text-start pt-5 border-top">
-            <div class="col-md-12">
-              <div class="custom-pagination">
-                <span>1</span>
-                <a href="#">2</a>
-                <a href="#">3</a>
-                <a href="#">4</a>
-                <span>...</span>
-                <a href="#">15</a>
-              </div>
-            </div>
-          </div>
+        <?php include("includes/pagination.php"); ?>
+        
         </div>
 
       <?php include("includes/sidebar.php"); ?>
